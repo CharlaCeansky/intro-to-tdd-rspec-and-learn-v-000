f567adb5b2090d7ab56fb90d4e2e2e725267fdab
Charla CCSky <ceansky@gmail.com>
2017-11-05 22:45:27 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,4 +1,4 @@
-def age_of_person_current_age_for_birth_year(year_of_birth)
+def current_age_for_birth_year(year_of_birth)
   year_of_birth = 1984
  return "The age of the person is #{2017 - year_of_birth}."
 end
