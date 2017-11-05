57ee655e995c6a3a1323801c15146e28b77268f0
Charla CCSky <ceansky@gmail.com>
2017-11-05 22:27:35 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,4 +1,4 @@
 def ageOfPerson = CurrentAgeForBirthYear(yearOfBirth)
 yearOfBirth = 1984
- return "The age of a person  is #{2017 - yearOfBirth}."
+ return "The age of a person  is {2017 - yearOfBirth}."
 end
